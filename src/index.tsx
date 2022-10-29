9532c00bfb61c830ec515737b958a44c78bd6422
eroux <elie.roux@telecom-bretagne.eu>
2022-10-29 22:15:08 +0200

    moving forward a bit

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React, { FC } from "react"
 import * as rdf from "rdflib"
 import { createRoot } from 'react-dom/client';
 import { RecoilRoot } from "recoil"
-import { BrowserRouter, Route, Routes, useLocation, useParams } from "react-router-dom"
+import { BrowserRouter, Route, Routes, useLocation, useParams, Link } from "react-router-dom"
 import { demoConfig } from "./demo_rde_config"
 import {
   EntityEditContainer,
@@ -14,15 +14,16 @@ import {
   IdTypeParams,
 } from "rdf-document-editor"
 
-import "rdf-document-editor/dist/index.css"
-
-const SimpleContainer: FC<{  }> = ({
+const HomeContainer: FC<{  }> = ({
 }) => {
   return (
     <React.Fragment>
-      <div>
-        <>{rdf.sym("http://example.com/ex").toString()}</>
-      </div>
+      <Link to="/new">
+        New entity    
+      </Link>
+      <Link to="/edit/bdr:P1583/bds:PersonUIShapes">
+        Load demo record    
+      </Link>
     </React.Fragment>
   )
 }
@@ -32,8 +33,35 @@ function App() {
     <BrowserRouter>
       <RecoilRoot>
         <Routes>
-          <Route path="/" element={<SimpleContainer />} />
-          <Route path="/new" element={<NewEntityContainer config={demoConfig} />} />
+              <Route path="/" element={<HomeContainer />} />
+              <Route path="/new" element={<NewEntityContainer config={demoConfig} />} />
+              <Route path="/new/:shapeQname" element={<EntityCreationContainer config={demoConfig} />} />
+              <Route // we need that route to link back value to property where entity was created
+                path="/new/:shapeQname/:subjectQname/:propertyQname/:index"
+                element={<EntityCreationContainerRoute config={demoConfig} />}
+              />
+              <Route // this one as well
+                path="/new/:shapeQname/:subjectQname/:propertyQname/:index/:subnodeQname"
+                element={<EntityCreationContainerRoute config={demoConfig} />}
+              />
+              <Route // same with entityQname
+                path="/new/:shapeQname/:subjectQname/:propertyQname/:index/named/:entityQname"
+                element={<EntityCreationContainerRoute config={demoConfig} />}
+              />
+              <Route // same with entityQname
+                path="/new/:shapeQname/:subjectQname/:propertyQname/:index/:subnodeQname/named/:entityQname"
+                element={<EntityCreationContainerRoute config={demoConfig} />}
+              />
+              <Route
+                path="/edit/:entityQname/:shapeQname/:subjectQname/:propertyQname/:index"
+                element={<EntityEditContainerMayUpdate config={demoConfig} />}
+              />
+              <Route
+                path="/edit/:entityQname/:shapeQname/:subjectQname/:propertyQname/:index/:subnodeQname"
+                element={<EntityEditContainerMayUpdate config={demoConfig} />}
+              />
+              <Route path="/edit/:entityQname/:shapeQname" element={<EntityEditContainer config={demoConfig} />} />
+              <Route path="/edit/:entityQname" element={<EntityShapeChooserContainer config={demoConfig} />} />
         </Routes>
       </RecoilRoot>
     </BrowserRouter>
